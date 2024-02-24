a5f2b9079b16aa41e5e410fa62deeaa0b6466844
TudorOrban <130213626+TudorOrban@users.noreply.github.com>
2024-02-24 22:39:52 +0200

    Add compute_shifts and end_of_file

--- src/storage_engine/index/index_writer.rs
+++ src/storage_engine/index/index_writer.rs
@@ -4,7 +4,7 @@ pub fn add_index_offsets(complete_inserted_rows: &Vec<Vec<String>>, schema_name:
   let row_index = RowIndex {
       row_offsets = utils::trace(row_columm_shift);
   };
-  let row_index_string = serde:to_string(row_index);
+  let row_index_string = serde::to_string(row_index);
   let row_index_file_path = get_row_index_file_path(&schema_name, &table_name);
   fs:write(row_index_file_path, row_index_file_path)?;
   
@@ -30,5 +30,22 @@ pub fn add_index_offsets(complete_inserted_rows: &Vec<Vec<String>>, schema_name:
 }
 
 fn compute_total_shift(complete_inserted_rows: &Vec<Vec<RowColumn>>, end_of_file_path) -> Vec<Vec<u32>> {
-  
+    let mut results: Vec<Vec<u32>> = Vec::new();
+
+    for row in complete_inserted_rows {
+      let results_row = row.iter().map(|value| (value + ", ").length() + end_of_file_path).collect();
+      results.push(results_row);
+    };
+    
+    results
 };
+
+fn get_end_of_file_offset(schema_name: &String, table_name: &String) -> Result<u64, Error> {
+    let file_path = get_table_row_index_file_path(&schema_name, &table_name);
+    let content = Fs::read(file_path)?;
+    let table_row_index = RowIndex {
+         row_offsets: serde_json::deserialize(content)
+    };
+    
+    table_row_index.row_offsets.get(-i)
+}
